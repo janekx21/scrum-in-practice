0e7038822deb2be68a6d6035a7ffa24e77cbbaf3
Janek Winkler <janekx21@gmail.de>
2025-12-10 18:27:33 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Node.js development environment with pnpm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            corepack
+          ];
+        };
+      }
+    );
+}
