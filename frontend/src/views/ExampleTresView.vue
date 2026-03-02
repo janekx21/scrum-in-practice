60c8988352688edc54a16b182092845167b1fea6
Janek Winkler <janekx21@gmail.de>
2026-03-02 13:58:23 +0100

    extract new 3D view

---
+++ frontend/src/views/ExampleTresView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import FirstExperience from '../components/FirstExperience.vue'
+    // window-size
+</script>
+
+<template>
+  <div style="width: 100%; height: 800px;">
+    <TresCanvas
+      clear-color="#82DBC5"
+      shadows
+      alpha
+      window-size
+      power-preference="high-performance"
+      preserve-drawing-buffer
+      physically-correct-lights
+      >
+
+      <FirstExperience />
+    </TresCanvas>
+  </div>
+</template>
