48453346dd767da7d01edfacfb34aaff63727702
akshatdaruka <akshatdaruka@gmail.com>
2026-03-02 23:59:22 +0100

    in progress, 3d view

--- frontend/src/views/FPSView.vue
+++ frontend/src/views/FPSView.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="fps-container">
-    <div v-if="store.isLoading" class="loader">Processing 3D Scan...</div>
-    <!-- The team member implementing three.js will put their Canvas component here -->
-    <div id="three-canvas-container" v-else-if="store.currentScan">
-       <!-- Data is now ready in store.currentScan -->
-    </div>
+  <div class="fps-container" style="height: calc(100vh - 56px); width: 100%; background: #000;">
+    <TresCanvas clear-color="#111" shadows alpha>
+      <!-- The camera starts here, but GLTFExample will move it to the right spot -->
+      <TresPerspectiveCamera :position="[10, 10, 10]" />
+      <OrbitControls make-default />
+
+      <TresAmbientLight :intensity="2" />
+      <TresDirectionalLight :position="[10, 10, 10]" :intensity="2" />
+
+      <Suspense>
+        <GLTFExample v-if="store.scanInfo" :path="store.scanInfo.modelUrl" />
+      </Suspense>
+      
+      <TresGridHelper :args="[200, 200, '#444', '#222']" />
+    </TresCanvas>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useThreeStore } from '@/stores/useThreeStore'
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+import GLTFExample from '@/components/GLTFExample.vue'
 
 const store = useThreeStore()
-
-onMounted(() => {
-  store.loadScan('sample_building')
-})
+onMounted(() => store.loadScan('sample_building'))
 </script>
