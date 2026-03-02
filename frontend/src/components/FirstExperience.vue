9256947235f85c579a920030c8626de1ef854748
Janek Winkler <janekx21@gmail.de>
2026-03-02 11:55:46 +0100

    add web view with example cube

---
+++ frontend/src/components/FirstExperience.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useLoop } from '@tresjs/core'
+import { ref } from 'vue'
+import { OrbitControls, useTweakPane } from '@tresjs/cientos';
+import GLTFExample from './GLTFExample.vue'
+
+//// Reference to our donut mesh for animation
+//const donutRef = ref()
+//
+//// Animation loop
+//const { onBeforeRender } = useLoop()
+//
+//onBeforeRender(({ elapsed }) => {
+//  if (donutRef.value) {
+//    // Rotate the donut on both X and Y axes
+//    donutRef.value.rotation.x = elapsed * 0.5
+//    donutRef.value.rotation.y = elapsed * 0.3
+//  }
+//})
+</script>
+
+<template>
+
+  <OrbitControls />
+
+  <!-- Camera Setup -->
+    <TresPerspectiveCamera
+      :position="[1, 2, 5]"
+      :fov="45"
+      :aspect="1"
+      :near="0.1"
+      :far="1000"
+    />
+
+  <TresScene>
+    <TresDirectionalLight :position="[0, 2, 4]" :intensity="2" cast-shadow />
+
+  <!-- The Donut -->
+    <TresMesh ref="donutRef" :position="[0, 2, 0]">
+      <TresTorusGeometry :args="[1, 0.4, 16, 32]" />
+      <!-- <TresMeshBasicMaterial color="#ff6b35" /> -->
+    </TresMesh>
+
+    <GLTFExample/>
+  </TresScene>
+
+</template>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,22 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import FirstExperience from '../components/FirstExperience.vue'
+    // window-size
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div style="width: 100%; height: 800px;">
+    <TresCanvas
+      clear-color="#82DBC5"
+      shadows
+      alpha
+      window-size
+      power-preference="high-performance"
+      preserve-drawing-buffer
+      physically-correct-lights
+      >
+
+      <FirstExperience />
+    </TresCanvas>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
