931d275fbba93369eb33dfc82c8525b60acdddfc
Janek Winkler <janekx21@gmail.de>
2026-03-04 10:55:22 +0100

    fix scan rotation & color

--- frontend/src/components/GLTFExample.vue
+++ frontend/src/components/GLTFExample.vue
@@ -3,6 +3,7 @@ import { useLoader } from '@tresjs/core'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import { watch } from 'vue'
+import * as THREE from 'three'
 
 const props = defineProps<{
   path: string
@@ -23,6 +24,23 @@ const { state: model, error } = useLoader(
   },
 )
 
+
+watch(model, (newModel) => {
+  if (newModel) {
+    newModel.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        const oldMat = child.material
+
+        // Fix the material properties
+        child.material = child.material.clone()
+        child.material.metalness = 0
+        child.material.roughness = 1
+        child.material.envMapIntensity = null
+      }
+    })
+  }
+})
+
 watch(error, (e) => {
   if (e) console.error("3D Model Loading Error:", e)
 })
@@ -30,8 +48,10 @@ watch(error, (e) => {
 
 <template>
   <!-- Render the entire scene from the file, not just one node -->
-  <primitive 
-    v-if="model" 
-    :object="model.scene" 
-  />
+  <primitive
+    v-if="model"
+    :object="model.scene"
+    :rotation="[-Math.PI / 2, 0, 0]"
+  >
+  </primitive>
 </template>
